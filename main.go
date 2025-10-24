fcd00e5e282bfddf057a90c0dc07c69b49cfa530
zhaokuang <zhaokuangDK@163.com>
2025-10-24 10:22:17 +0800

    add:在main中添加优雅关闭功能

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"ismismcube-backend/internal/api"
 	"ismismcube-backend/internal/config"
@@ -13,14 +17,35 @@ import (
 
 func main() {
 	config.Init()
-
 	task_manager.InitTaskManager(&ws.WebSocketBroadcaster{})
-
 	api.Init()
 
-	log.Printf("Server is running at http://127.0.0.1:%s", config.Port)
-	if err := http.ListenAndServe(":"+config.Port, nil); err != nil {
-		log.Fatal("Failed to start server:", err)
-		os.Exit(1)
+	server := &http.Server{
+		Addr:    ":" + config.Port,
+		Handler: nil, // 使用默认的 DefaultServeMux
 	}
+	go func() {
+		log.Printf("Server is running at http://127.0.0.1:%s", config.Port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Server forced to shutdown: %v", err)
+	} else {
+		log.Println("Server shutdown completed")
+	}
+	config.CloseDB()
+	log.Println("Database connection closed")
+
+	log.Println("--------------------------")
+	log.Println("Server exited gracefully")
 }
